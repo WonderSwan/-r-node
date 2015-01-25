f1c7ab8283fa8916a2620840d5a71f5badf08dab
Stephen Sauceda <contact@stephensauceda.com>
2015-01-25 16:15:32 -0500

    clean up and formatting

--- server.js
+++ server.js
@@ -4,19 +4,18 @@ var http = require( 'http' );
 var bot = require ( './lib/bot' );
 
 // call reddit bot every hour
-setInterval(function(){
+setInterval( function(){
   bot();
-}, 3600000 * 2); // two hours
+}, 3600000 * 2 ); // two hours
 
 // run it on start up also
 bot();
 
 // keep the app from falling asleep
 if ( 'production' === process.env.NODE_ENV ){
-  setInterval(function() {
-    http.get('http://rnode.herokuapp.com');
-  }, 300000); // ping the app every five minutes
-
+  setInterval( function() {
+    http.get( 'http://rnode.herokuapp.com' );
+  }, 300000 ); // ping the app every five minutes
 }
 
 app.listen( process.env.PORT || 3000 );
